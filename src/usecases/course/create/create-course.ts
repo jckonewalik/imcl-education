c6554cb0fbde0b84582c6c7b82b0f7b88faa63c7
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-17 18:44:28 -0300

    feat: create course use case

---
+++ src/usecases/course/create/create-course.ts
@@ -0,0 +1,15 @@
+import Course from "../../../domain/course/entity/course";
+import CourseRepository from "../../../domain/course/repository/course.repository";
+import { CreateCourseDto } from "./create-course.dto";
+import { v4 as uuid } from "uuid";
+
+export default class CreateCourseUseCase {
+  constructor(private readonly repository: CourseRepository) {}
+
+  async create(dto: CreateCourseDto): Promise<Course> {
+    const course = new Course(uuid(), dto.name, true);
+
+    await this.repository.create(course);
+    return course;
+  }
+}
