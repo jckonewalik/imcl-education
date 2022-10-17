2e88cc22aceeb93d4bfb17ab93e8b126d28cee7f
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-17 16:28:25 -0300

    feat: student class entity

---
+++ src/domain/student-class/entity/student-class.ts
@@ -0,0 +1,44 @@
+import BadRequestException from "../../exceptions/bad-request-exception";
+import InvalidValueException from "../../exceptions/invalid-value-exception";
+import Messages from "../../util/messages";
+
+export default class StudentClass {
+  private _id: string;
+  private _courseId: string;
+  private _name: string;
+  private _active: boolean;
+
+  constructor(id: string, courseId: string, name: string, active: boolean) {
+    if (!id) {
+      throw new InvalidValueException(Messages.MISSING_STUDENT_CLASS_ID);
+    }
+    if (!courseId) {
+      throw new InvalidValueException(Messages.MISSING_COURSE_ID);
+    }
+    if (!name) {
+      throw new InvalidValueException(Messages.MISSING_STUDENT_CLASS_NAME);
+    }
+    this._id = id;
+    this._courseId = courseId;
+    this._name = name;
+    this._active = active;
+  }
+
+  activate() {
+    if (this._active) {
+      throw new BadRequestException(Messages.CLASS_ALREADY_ACTIVE);
+    }
+    this._active = true;
+  }
+
+  inactivate() {
+    if (!this._active) {
+      throw new BadRequestException(Messages.CLASS_ALREADY_INACTIVE);
+    }
+    this._active = false;
+  }
+
+  get active(): boolean {
+    return this._active;
+  }
+}
--- src/domain/util/messages.ts
+++ src/domain/util/messages.ts
@@ -3,4 +3,8 @@ export default class Messages {
   static MISSING_COURSE_NAME = "Informe o nome do curso";
   static COURSE_ALREADY_ACTIVE = "O curso já esta ativo";
   static COURSE_ALREADY_INACTIVE = "O curso já esta inativo";
+  static MISSING_STUDENT_CLASS_ID = "Informe o ID da turma";
+  static MISSING_STUDENT_CLASS_NAME = "Informe o nome da turma";
+  static CLASS_ALREADY_ACTIVE = "A turma ja esta ativa";
+  static CLASS_ALREADY_INACTIVE = "A turma ja esta inativa";
 }
