9a7bf3165ad9ff0c271369d850b21a9ffcfdaa13
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-18 12:03:20 -0300

    test: update course use case

---
+++ src/usecases/course/update/__test__/update-course.spec.ts
@@ -0,0 +1,110 @@
+import { Course } from "@/domain/course/entity";
+import { v4 as uuid } from "uuid";
+import faker from "faker";
+import { UpdateCourseUseCase } from "../update-course";
+import {
+  FindCourseRepository,
+  UpdateCourseRepository,
+} from "@/domain/course/repository/course.repository";
+
+type Suts = {
+  findRepo: FindCourseRepository;
+  updateRepo: UpdateCourseRepository;
+  sut: UpdateCourseUseCase;
+};
+
+const makeSuts = (course: Course): Suts => {
+  const findRepo = {
+    async find(id: string): Promise<Course> {
+      return course;
+    },
+  };
+  const updateRepo = {
+    async update(course: Course): Promise<void> {},
+  };
+  return {
+    findRepo,
+    updateRepo,
+    sut: new UpdateCourseUseCase(findRepo, updateRepo),
+  };
+};
+describe("Update Course Use Case", () => {
+  it("updating course adding new lessons", async () => {
+    const course = new Course(uuid(), faker.name.jobArea(), true);
+    const { updateRepo, sut } = makeSuts(course);
+    const spyUpdate = jest.spyOn(updateRepo, "update");
+
+    const updatedCourse = await sut.update({
+      id: course.id,
+      lessons: [
+        {
+          name: faker.random.word(),
+          number: 1,
+          action: "A",
+        },
+        {
+          name: faker.random.word(),
+          number: 2,
+          action: "A",
+        },
+      ],
+    });
+    expect(spyUpdate).toHaveBeenCalledWith(updatedCourse);
+    expect(updatedCourse.lessons.length).toBe(2);
+  });
+
+  it("updating course removing lessons", async () => {
+    const course = new Course(uuid(), faker.name.jobArea(), true);
+    course.addLesson(1, faker.random.word());
+    course.addLesson(2, faker.random.word());
+
+    const lesson1 = course.lessons[0];
+
+    const { updateRepo, sut } = makeSuts(course);
+    const spyUpdate = jest.spyOn(updateRepo, "update");
+
+    const updatedCourse = await sut.update({
+      id: course.id,
+      lessons: [
+        {
+          id: lesson1.id,
+          name: lesson1.name,
+          number: lesson1.number,
+          action: "D",
+        },
+      ],
+    });
+    expect(spyUpdate).toHaveBeenCalledWith(updatedCourse);
+    expect(updatedCourse.lessons.length).toBe(1);
+  });
+
+  it("updating course inactivating a lesson", async () => {
+    const course = new Course(uuid(), faker.name.jobArea(), true);
+    course.addLesson(1, faker.random.word());
+    course.addLesson(2, faker.random.word());
+
+    const lesson1 = course.lessons[0];
+
+    const { updateRepo, sut } = makeSuts(course);
+    const spyUpdate = jest.spyOn(updateRepo, "update");
+
+    const updatedCourse = await sut.update({
+      id: course.id,
+      lessons: [
+        {
+          id: lesson1.id,
+          name: lesson1.name,
+          number: lesson1.number,
+          action: "I",
+        },
+      ],
+    });
+    expect(spyUpdate).toHaveBeenCalledWith(updatedCourse);
+    expect(updatedCourse.lessons.length).toBe(2);
+    const updatedLesson = updatedCourse.lessons.find(
+      (l) => l.id === lesson1.id
+    );
+    expect(updatedLesson).toBeDefined();
+    expect(updatedLesson?.active).toBe(false);
+  });
+});
