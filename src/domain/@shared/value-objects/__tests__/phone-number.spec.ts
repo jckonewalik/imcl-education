6b7ada351feab0c17396690fc3ac7a8f2fd34d67
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-18 17:30:45 -0300

    test: phone number vo

---
+++ src/domain/@shared/value-objects/__tests__/phone-number.spec.ts
@@ -0,0 +1,32 @@
+import { InvalidValueException } from "../../exceptions";
+import { PhoneNumber } from "../phone-number";
+import faker from "faker";
+
+describe("Phone Number unit tests", () => {
+  describe("Phone Number invalid scenarios", () => {
+    test.each`
+      a                         | expected
+      ${"1234"}                 | ${InvalidValueException}
+      ${"(43)9999-9999"}        | ${InvalidValueException}
+      ${"45999999999999999999"} | ${InvalidValueException}
+    `("$expected when $a ", ({ a, expected }) => {
+      const t = () => {
+        new PhoneNumber(a, true);
+      };
+      expect(t).toThrow(expected);
+    });
+  });
+
+  describe("Phone Number valid scenarios", () => {
+    test.each`
+      number            | isWhatsapp | expected
+      ${"043999999999"} | ${true}    | ${"43999999999"}
+      ${"4399999999"}   | ${false}   | ${"4399999999"}
+    `("$expected when $number ", ({ number, isWhatsapp, expected }) => {
+      const phone = new PhoneNumber(number, isWhatsapp);
+
+      expect(phone.number).toBe(expected);
+      expect(phone.isWhatsapp).toBe(isWhatsapp);
+    });
+  });
+});
