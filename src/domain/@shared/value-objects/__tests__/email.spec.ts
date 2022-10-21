abac42cf18e7d89ab829e326d0ad648f0f3dd1ae
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-21 09:24:43 -0300

    test: email vo

---
+++ src/domain/@shared/value-objects/__tests__/email.spec.ts
@@ -0,0 +1,31 @@
+import { InvalidValueException } from "../../exceptions";
+import { Email } from "../email";
+
+describe("Email unit tests", () => {
+  describe("Email invalid scenarios", () => {
+    test.each`
+      a               | expected
+      ${"test"}       | ${InvalidValueException}
+      ${"teste@test"} | ${InvalidValueException}
+      ${"teste.com"}  | ${InvalidValueException}
+      ${"@test.com"}  | ${InvalidValueException}
+    `("$expected when $a ", ({ a, expected }) => {
+      const t = () => {
+        new Email(a);
+      };
+      expect(t).toThrow(expected);
+    });
+  });
+
+  describe("Email valid scenarios", () => {
+    test.each`
+      value                   | expected
+      ${"teste@teste.com"}    | ${"teste@teste.com"}
+      ${"teste@teste.com.br"} | ${"teste@teste.com.br"}
+    `("$expected when $value ", ({ value, expected }) => {
+      const email = new Email(value);
+
+      expect(email.value).toBe(expected);
+    });
+  });
+});
