193f6fa82b8f0c4e4bd7691cda7bc85d395d4499
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-21 09:27:28 -0300

    test: teacher entity

---
+++ src/domain/teacher/entity/__test__/teacher.spec.ts
@@ -0,0 +1,28 @@
+import { InvalidValueException } from "@/domain/@shared/exceptions";
+import Messages from "@/domain/@shared/util/messages";
+import { Teacher } from "../teacher";
+import faker from "faker";
+import { v4 as uuid } from "uuid";
+import { Gender } from "@/domain/@shared/enums/gender";
+import { Email } from "@/domain/@shared/value-objects";
+
+describe("Teacher Unit tests", () => {
+  it("Fail creating a new Teacher without an ID", () => {
+    const email = new Email(faker.internet.email());
+    const t = () => {
+      new Teacher("", faker.name.firstName(), Gender.F, email, true);
+    };
+    expect(t).toThrow(InvalidValueException);
+    expect(t).toThrow(Messages.MISSING_TEACHER_ID);
+  });
+
+  it("Fail creating a new Teacher without a name", () => {
+    const email = new Email(faker.internet.email());
+
+    const t = () => {
+      new Teacher(uuid(), "", Gender.F, email, true);
+    };
+    expect(t).toThrow(InvalidValueException);
+    expect(t).toThrow(Messages.MISSING_TEACHER_NAME);
+  });
+});
