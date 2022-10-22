13a5976498e0eeb5f60edccecc9919dc56fdba68
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-22 15:38:54 -0300

    test: activate and inactivate teachers

--- src/domain/teacher/entity/__test__/teacher.spec.ts
+++ src/domain/teacher/entity/__test__/teacher.spec.ts
@@ -1,10 +1,24 @@
-import { InvalidValueException } from "@/domain/@shared/exceptions";
+import {
+  BadRequestException,
+  InvalidValueException,
+} from "@/domain/@shared/exceptions";
 import Messages from "@/domain/@shared/util/messages";
 import { Teacher } from "../teacher";
 import faker from "faker";
 import { v4 as uuid } from "uuid";
 import { Gender } from "@/domain/@shared/enums/gender";
 import { Email } from "@/domain/@shared/value-objects";
+
+const makeTeacher = ({
+  id = uuid(),
+  name = faker.name.firstName(),
+  gender = Gender.M,
+  email = faker.internet.email(),
+  active = true,
+}): Teacher => {
+  const teacher = new Teacher(id, name, gender, new Email(email), active);
+  return teacher;
+};
 
 describe("Teacher Unit tests", () => {
   it("Fail creating a new Teacher without an ID", () => {
@@ -27,19 +41,52 @@ describe("Teacher Unit tests", () => {
   });
 
   it("Fail changing the name with an invalid name", () => {
-    const email = new Email(faker.internet.email());
-    const teacher = new Teacher(
-      uuid(),
-      faker.name.firstName(),
-      Gender.F,
-      email,
-      true
-    );
+    const teacher = makeTeacher({});
 
     const t = () => {
       teacher.changeName("");
     };
     expect(t).toThrow(InvalidValueException);
     expect(t).toThrow(Messages.MISSING_TEACHER_NAME);
+  });
+
+  it("Change teacher's name", () => {
+    const teacher = makeTeacher({});
+    const newName = faker.name.firstName();
+    teacher.changeName(newName);
+    expect(teacher.name).toBe(newName);
+  });
+
+  it("Fail when activate a teacher already active", () => {
+    const teacher = makeTeacher({});
+    const t = () => {
+      teacher.activate();
+    };
+    expect(t).toThrow(BadRequestException);
+    expect(t).toThrow(Messages.TEACHER_ALREADY_ACTIVE);
+  });
+
+  it("Fail when inactivate a teacher already inactive", () => {
+    const teacher = makeTeacher({ active: false });
+
+    const t = () => {
+      teacher.inactivate();
+    };
+    expect(t).toThrow(BadRequestException);
+    expect(t).toThrow(Messages.TEACHER_ALREADY_INACTIVE);
+  });
+
+  it("Activate teacher", () => {
+    const teacher = makeTeacher({ active: false });
+
+    teacher.activate();
+    expect(teacher.active).toBe(true);
+  });
+
+  it("Inactivate teacher", () => {
+    const teacher = makeTeacher({ active: true });
+
+    teacher.inactivate();
+    expect(teacher.active).toBe(false);
   });
 });
