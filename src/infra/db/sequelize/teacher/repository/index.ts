b0a85b9237531c7ef0b02a8443f65bb06be1876d
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-24 09:27:38 -0300

    feat: sequelize update teacher repository

--- src/infra/db/sequelize/teacher/repository/index.ts
+++ src/infra/db/sequelize/teacher/repository/index.ts
@@ -1,2 +1,4 @@
 export * from "./create-teacher.repository";
 export * from "./find-teacher-by-email.repository";
+export * from "./find-teacher.repository";
+export * from "./update-teacher.repository";
---
+++ src/infra/db/sequelize/teacher/repository/update-teacher.repository.ts
@@ -0,0 +1,24 @@
+import { Teacher } from "@/domain/teacher/entity";
+import { UpdateTeacherRepository } from "@/domain/teacher/repository";
+import { TeacherModel } from "../model/teacher.model";
+
+export class SequelizeUpdateTeacherRepository
+  implements UpdateTeacherRepository
+{
+  async update(entity: Teacher): Promise<void> {
+    await TeacherModel.update(
+      {
+        id: entity.id,
+        name: entity.name,
+        gender: entity.gender.toString(),
+        email: entity.email.value,
+        active: entity.active,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
+  }
+}
