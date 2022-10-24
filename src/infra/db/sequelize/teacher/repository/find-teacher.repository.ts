0ceb70ba3ba91e87c537a640d062817f444b4acc
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-24 09:17:06 -0300

    feat: sequelize find teacher repository

---
+++ src/infra/db/sequelize/teacher/repository/find-teacher.repository.ts
@@ -0,0 +1,17 @@
+import { Teacher } from "@/domain/teacher/entity";
+import { FindTeacherRepository } from "@/domain/teacher/repository";
+import { TeacherModel } from "../model/teacher.model";
+
+export class SequelizeFindTeacherRepository implements FindTeacherRepository {
+  async find(id: string): Promise<Teacher | undefined> {
+    const teacherModel = await TeacherModel.findOne({
+      where: { id },
+    });
+
+    if (!teacherModel) {
+      return undefined;
+    }
+
+    return teacherModel.toEntity();
+  }
+}
