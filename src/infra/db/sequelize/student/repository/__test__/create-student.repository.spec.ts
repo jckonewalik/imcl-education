c524211833941eedd01336a43eb6eac2beaeb8de
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-24 09:45:17 -0300

    test: sequelize create student repository

---
+++ src/infra/db/sequelize/student/repository/__test__/create-student.repository.spec.ts
@@ -0,0 +1,45 @@
+import { Sequelize } from "sequelize-typescript";
+import { StudentModel } from "../../model/student.model";
+import { SequelizeCreateStudentRepository } from "../create-student.repository";
+import { makeStudent } from "./util";
+
+describe("Sequelize Create Student Repository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([StudentModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("Create a student", async () => {
+    const repository = new SequelizeCreateStudentRepository();
+    const student = makeStudent();
+
+    await repository.create(student);
+
+    const studentModel = await StudentModel.findOne({
+      where: { id: student.id },
+    });
+
+    expect(studentModel).toBeDefined();
+    expect(studentModel?.id).toBe(student.id);
+    expect(studentModel?.name).toBe(student.name);
+    expect(studentModel?.gender).toBe(student.gender.toString());
+    expect(studentModel?.phoneNumber).toBe(student.phone?.number);
+    expect(studentModel?.phoneIsWhatsapp).toBe(student.phone?.isWhatsapp);
+    expect(studentModel?.active).toBe(student.active);
+    expect(studentModel?.creationDate).toBeDefined();
+    expect(studentModel?.updatedOn).toBeDefined();
+  });
+});
