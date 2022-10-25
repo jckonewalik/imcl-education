f15e7d2aa10ba36bff843033dd19bd001950b61b
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-25 16:10:04 -0300

    test: sequelize update course repository

---
+++ src/infra/db/sequelize/course/repository/__test__/update-course.repository.spec.ts
@@ -0,0 +1,68 @@
+import { Sequelize } from "sequelize-typescript";
+import { CourseModel } from "../../model/course.model";
+import { LessonModel } from "../../model/lesson.model";
+import { SequelizeUpdateCourseRepository } from "../update-course.repository";
+import { makeCourse } from "./util";
+import faker from "faker";
+describe("Sequelize Update Course Repository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([CourseModel, LessonModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("Update a course", async () => {
+    const repository = new SequelizeUpdateCourseRepository();
+    const course = makeCourse();
+    await CourseModel.create(
+      {
+        id: course.id,
+        name: course.name,
+        active: course.active,
+        lessons: course.lessons.map((l) => ({
+          id: l.id,
+          courseId: l.courseId,
+          number: l.number,
+          name: l.name,
+          active: l.active,
+        })),
+      },
+      { include: "lessons" }
+    );
+
+    const lessons = course.lessons;
+    course.inactivate();
+    course.removeLesson(lessons[1]);
+    course.lessons[0].inactivate();
+    course.addLesson(3, faker.random.word());
+
+    await repository.update(course);
+
+    const courseModel = await CourseModel.findOne({
+      where: { id: course.id },
+      include: "lessons",
+    });
+
+    expect(courseModel).toBeDefined();
+    expect(courseModel?.id).toBe(course.id);
+    expect(courseModel?.name).toBe(course.name);
+    expect(courseModel?.active).toBe(course.active);
+    expect(courseModel?.lessons.length).toBe(2);
+    expect(courseModel?.lessons.filter((l) => l.active).length).toBe(1);
+    expect(courseModel?.lessons.filter((l) => !l.active).length).toBe(1);
+    expect(courseModel?.creationDate).toBeDefined();
+    expect(courseModel?.updatedOn).not.toEqual(courseModel?.creationDate);
+  });
+});
