97b9e43d27c22896df66f97238f24ca278d5db8d
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-25 16:09:24 -0300

    feat: sequelize update course repository

---
+++ src/infra/db/sequelize/course/repository/update-course.repository.ts
@@ -0,0 +1,54 @@
+import { Course } from "@/domain/course/entity";
+import { UpdateCourseRepository } from "@/domain/course/repository";
+import { CourseModel } from "../model/course.model";
+import { LessonModel } from "../model/lesson.model";
+
+export class SequelizeUpdateCourseRepository implements UpdateCourseRepository {
+  async update(entity: Course): Promise<void> {
+    const t = await CourseModel.sequelize?.transaction();
+    try {
+      const lessons = entity.lessons;
+      let lessonModels = await LessonModel.findAll({
+        where: { courseId: entity.id },
+      });
+
+      for (const lesson of lessons) {
+        const exists = lessonModels.find((m) => m.id === lesson.id);
+        if (exists) {
+          await exists.update({
+            active: lesson.active,
+          });
+        } else {
+          await LessonModel.create({
+            id: lesson.id,
+            courseId: lesson.courseId,
+            number: lesson.number,
+            name: lesson.name,
+            active: lesson.active,
+          });
+        }
+        lessonModels = lessonModels.filter((m) => m.id !== lesson.id);
+      }
+      for (const lesson of lessonModels) {
+        LessonModel.destroy({
+          where: { id: lesson.id },
+        });
+      }
+      await CourseModel.update(
+        {
+          id: entity.id,
+          name: entity.name,
+          active: entity.active,
+        },
+        {
+          where: {
+            id: entity.id,
+          },
+        }
+      );
+      await t?.commit();
+    } catch (error) {
+      await t?.rollback();
+    }
+  }
+}
