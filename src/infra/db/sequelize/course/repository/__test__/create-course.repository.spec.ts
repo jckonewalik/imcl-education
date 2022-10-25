a1fc428928a223808ded3aec03baec328a0d8a5a
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-10-25 15:16:15 -0300

    test: sequelize create course repository

---
+++ src/infra/db/sequelize/course/repository/__test__/create-course.repository.spec.ts
@@ -0,0 +1,51 @@
+import { Course } from "@/domain/course/entity";
+import { Sequelize } from "sequelize-typescript";
+import { CourseModel } from "../../model/course.model";
+import { LessonModel } from "../../model/lesson.model";
+import { SequelizeCreateCourseRepository } from "../create-course.repository";
+import { v4 as uuid } from "uuid";
+import faker from "faker";
+describe("Sequelize Create Course Repository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([CourseModel, LessonModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("Create a course with lessons", async () => {
+    const repository = new SequelizeCreateCourseRepository();
+    const course = new Course(uuid(), faker.random.word(), true);
+    course.addLesson(1, faker.random.word());
+
+    await repository.create(course);
+
+    const courseModel = await CourseModel.findOne({
+      where: { id: course.id },
+      include: "lessons",
+    });
+
+    expect(courseModel).toBeDefined();
+    console.log(courseModel?.toJSON());
+    expect(courseModel?.id).toBe(course.id);
+    expect(courseModel?.name).toBe(course.name);
+    expect(courseModel?.lessons.length).toBe(1);
+    expect(courseModel?.lessons[0].id).toBe(course.lessons[0].id);
+    expect(courseModel?.lessons[0].number).toBe(course.lessons[0].number);
+    expect(courseModel?.lessons[0].name).toBe(course.lessons[0].name);
+    expect(courseModel?.lessons[0].active).toBe(course.lessons[0].active);
+    expect(courseModel?.creationDate).toBeDefined();
+    expect(courseModel?.updatedOn).toBeDefined();
+  });
+});
