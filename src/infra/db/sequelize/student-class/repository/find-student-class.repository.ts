f372e3a18f38628f7d118e7a63b0ede376a8159c
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-11-11 17:40:05 -0300

    feat: validate uuid on find student class repository

--- src/infra/db/sequelize/student-class/repository/find-student-class.repository.ts
+++ src/infra/db/sequelize/student-class/repository/find-student-class.repository.ts
@@ -1,11 +1,16 @@
+import { BadRequestException } from "@/domain/@shared/exceptions";
+import Messages from "@/domain/@shared/util/messages";
 import { StudentClass } from "@/domain/student-class/entity";
 import { FindStudentClassRepository } from "@/domain/student-class/repository";
+import { validate } from "uuid";
 import { StudentClassModel } from "../model";
-
 export class SequelizeFindStudentClassRepository
   implements FindStudentClassRepository
 {
   async find(id: string): Promise<StudentClass | undefined> {
+    if (!validate(id)) {
+      throw new BadRequestException(Messages.INVALID_ID);
+    }
     const studentClass = await StudentClassModel.findOne({
       where: { id },
       include: ["teachers", "enrollments"],
