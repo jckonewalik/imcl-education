89604e22156d6c7c9a34010c8a77d22d9901e436
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-11-11 17:51:26 -0300

    feat: validate uuid on find course repository

--- src/infra/db/sequelize/course/repository/find-course.repository.ts
+++ src/infra/db/sequelize/course/repository/find-course.repository.ts
@@ -1,9 +1,15 @@
+import { BadRequestException } from "@/domain/@shared/exceptions";
+import Messages from "@/domain/@shared/util/messages";
 import { Course } from "@/domain/course/entity";
 import { FindCourseRepository } from "@/domain/course/repository";
+import { validate } from "uuid";
 import { CourseModel } from "../model";
 
 export class SequelizeFindCourseRepository implements FindCourseRepository {
   async find(id: string): Promise<Course | undefined> {
+    if (!validate(id)) {
+      throw new BadRequestException(Messages.INVALID_ID);
+    }
     const courseModel = await CourseModel.findOne({
       where: { id },
       include: "lessons",
