431104983bd7126cae4da723b5f3b5dc345e767f
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-11-11 17:52:58 -0300

    refactor: remove dto uuid validation

--- src/presentation/student-class/dto/create-student-class.dto.ts
+++ src/presentation/student-class/dto/create-student-class.dto.ts
@@ -1,14 +1,11 @@
 import Messages from "@/domain/@shared/util/messages";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsUUID } from "class-validator";
+import { IsNotEmpty } from "class-validator";
 
 export class CreateStudentClassDto {
   @ApiProperty({
     description: "ID do curso",
     required: true,
-  })
-  @IsUUID("4", {
-    message: Messages.INVALID_ID,
   })
   @IsNotEmpty({
     message: Messages.MISSING_COURSE_ID,
