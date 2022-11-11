ef4220c36efb9ae6f52be8d8cbed2a65ac277990
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-11-11 17:35:17 -0300

    feat: validate uuid on find teacher repository

--- src/infra/db/sequelize/teacher/repository/find-teacher.repository.ts
+++ src/infra/db/sequelize/teacher/repository/find-teacher.repository.ts
@@ -1,9 +1,14 @@
+import { BadRequestException } from "@/domain/@shared/exceptions";
+import Messages from "@/domain/@shared/util/messages";
 import { Teacher } from "@/domain/teacher/entity";
 import { FindTeacherRepository } from "@/domain/teacher/repository";
+import { validate } from "uuid";
 import { TeacherModel } from "../model";
-
 export class SequelizeFindTeacherRepository implements FindTeacherRepository {
   async find(id: string): Promise<Teacher | undefined> {
+    if (!validate(id)) {
+      throw new BadRequestException(Messages.INVALID_ID);
+    }
     const teacherModel = await TeacherModel.findOne({
       where: { id },
     });
