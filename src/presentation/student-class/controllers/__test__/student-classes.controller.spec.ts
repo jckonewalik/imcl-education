2cdc5f995b8752d2c5bbed94fc3ddd8d7689ff96
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-11-12 16:17:47 -0300

    test: student classes controller - update class API

--- src/presentation/student-class/controllers/__test__/student-classes.controller.spec.ts
+++ src/presentation/student-class/controllers/__test__/student-classes.controller.spec.ts
@@ -1,3 +1,4 @@
+import { Gender } from "@/domain/@shared/enums/gender";
 import Messages from "@/domain/@shared/util/messages";
 import { CourseModel, LessonModel } from "@/infra/db/sequelize/course/model";
 import {
@@ -14,6 +15,7 @@ import faker from "faker";
 import { Sequelize } from "sequelize-typescript";
 import request from "supertest";
 import { v4 as uuid } from "uuid";
+
 const createCourse = async (): Promise<CourseModel> => {
   const course = await CourseModel.create({
     id: uuid(),
@@ -21,6 +23,27 @@ const createCourse = async (): Promise<CourseModel> => {
     active: true,
   });
   return course;
+};
+
+const createTeacher = async (): Promise<TeacherModel> => {
+  const teacher = await TeacherModel.create({
+    id: uuid(),
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    email: `${faker.internet.email()}`,
+    gender: Gender.M,
+    active: true,
+  });
+  return teacher;
+};
+
+const createStudent = async (): Promise<StudentModel> => {
+  const student = await StudentModel.create({
+    id: uuid(),
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    gender: Gender.M,
+    active: true,
+  });
+  return student;
 };
 
 describe("Student Classes Controller Tests", () => {
@@ -90,6 +113,60 @@ describe("Student Classes Controller Tests", () => {
       .then((result) => {
         expect(result.statusCode).toEqual(400);
         expect(result._body.message).toEqual(Messages.MISSING_COURSE_ID);
+      });
+  });
+
+  it(`/PUT student-classes`, async () => {
+    const course = await createCourse();
+    const student = await createStudent();
+    const teacher = await createTeacher();
+    const studentClass = await StudentClassModel.create({
+      id: uuid(),
+      courseId: course.id,
+      name: faker.random.word(),
+      active: true,
+    });
+    const newName = faker.random.word();
+
+    const response = await request(app.getHttpServer())
+      .put(`/student-classes/${studentClass.id}`)
+      .send({
+        name: newName,
+        active: true,
+        students: [
+          {
+            studentId: student.id,
+            action: "A",
+          },
+        ],
+        teachers: [
+          {
+            teacherId: teacher.id,
+            action: "A",
+          },
+        ],
+      });
+
+    const result = await StudentClassModel.findOne({
+      where: { id: studentClass.id },
+      include: ["teachers", "enrollments"],
+    });
+    expect(result).toBeDefined();
+    expect(result?.name).toBe(newName);
+    expect(result?.teachers.length).toBe(1);
+    expect(result?.enrollments.length).toBe(1);
+  });
+
+  it(`404 /PUT student-classes with invalid student class`, async () => {
+    await request(app.getHttpServer())
+      .put(`/student-classes/${uuid()}`)
+      .send({
+        name: faker.random.word(),
+        active: false,
+      })
+      .then((result) => {
+        expect(result.statusCode).toEqual(404);
+        expect(result._body.message).toEqual(Messages.INVALID_STUDENT_CLASS);
       });
   });
 });
