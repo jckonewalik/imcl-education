6eb597d4b502c728001ab3527bea7d1a5b376081
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-11-24 14:25:25 -0300

    test: create class registry repository

---
+++ src/infra/db/sequelize/class-registry/repository/__test__/create-class-registry.repository.spec.ts
@@ -0,0 +1,86 @@
+import { ClassRegistry } from "@/domain/class-registry/entity";
+import { Sequelize } from "sequelize-typescript";
+import { v4 as uuid } from "uuid";
+import { CourseModel, LessonModel } from "../../../course/model";
+import {
+  EnrollmentModel,
+  StudentClassModel,
+  StudentClassTeacherModel,
+} from "../../../student-class/model";
+import { StudentModel } from "../../../student/model";
+import { TeacherModel } from "../../../teacher/model";
+import {
+  ClassRegistryLessonModel,
+  ClassRegistryModel,
+  ClassRegistryStudentModel,
+} from "../../model";
+import { SequelizeCreateClassRegistryRepository } from "../create-class-registry.repostitory";
+import { makeModels } from "./util";
+describe("Sequelize Create Class Registry Repository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([
+      StudentClassModel,
+      CourseModel,
+      EnrollmentModel,
+      StudentClassTeacherModel,
+      TeacherModel,
+      LessonModel,
+      StudentModel,
+      ClassRegistryModel,
+      ClassRegistryStudentModel,
+      ClassRegistryLessonModel,
+    ]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("Create a class registry", async () => {
+    const { course, student, teacher, studentClass } = await makeModels();
+
+    const repository = new SequelizeCreateClassRegistryRepository();
+
+    const registry = new ClassRegistry(
+      uuid(),
+      studentClass.id,
+      new Date(),
+      teacher.id,
+      [student.id],
+      [course.lessons[0].id]
+    );
+
+    await repository.create(registry);
+
+    const registryModel = await ClassRegistryModel.findOne({
+      where: { id: registry.id },
+      include: ["students", "lessons"],
+    });
+
+    expect(registryModel).not.toBeNull();
+    expect(registryModel?.id).toBe(registry.id);
+    expect(registryModel?.studentClassId).toBe(registry.studentClassId);
+    expect(registryModel?.teacherId).toBe(registry.teacherId);
+    expect(registryModel?.date).toStrictEqual(
+      registry.date.toISOString().split("T")[0]
+    );
+    expect(registryModel?.students.length).toBe(1);
+    expect(registryModel?.students[0].id).toBe(student.id);
+
+    expect(registryModel?.lessons.length).toBe(1);
+    expect(registryModel?.lessons[0].id).toBe(course.lessons[0].id);
+
+    expect(registryModel?.creationDate).toBeDefined();
+    expect(registryModel?.updatedOn).toBeDefined();
+  });
+});
---
+++ src/infra/db/sequelize/class-registry/repository/__test__/util/index.ts
@@ -0,0 +1,53 @@
+import { Gender } from "@/domain/@shared/enums/gender";
+import { CourseModel } from "@/infra/db/sequelize/course/model";
+import { StudentClassModel } from "@/infra/db/sequelize/student-class/model";
+import { StudentModel } from "@/infra/db/sequelize/student/model";
+import { TeacherModel } from "@/infra/db/sequelize/teacher/model";
+import faker from "faker";
+import { v4 as uuid } from "uuid";
+
+export const makeModels = async () => {
+  const courseId = uuid();
+  const course = await CourseModel.create(
+    {
+      id: courseId,
+      name: faker.random.word(),
+      active: true,
+      lessons: [
+        {
+          id: uuid(),
+          courseId: courseId,
+          number: 1,
+          name: faker.random.word(),
+          active: true,
+        },
+      ],
+    },
+    { include: "lessons" }
+  );
+
+  const teacher = await TeacherModel.create({
+    id: uuid(),
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    gender: Gender.F,
+    email: faker.internet.email(),
+    active: true,
+  });
+
+  const student = await StudentModel.create({
+    id: uuid(),
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    gender: Gender.M,
+    active: true,
+  });
+
+  const studentClass = await StudentClassModel.create({
+    id: uuid(),
+    courseId: course.id,
+    name: faker.random.word(),
+    year: new Date().getFullYear(),
+    active: true,
+  });
+
+  return { course, teacher, student, studentClass };
+};
