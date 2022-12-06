8177af585e4c951537b6f206553014b2ca83d9fb
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-12-06 12:49:07 -0300

    test: delete class registry repository

---
+++ src/infra/db/sequelize/class-registry/repository/__test__/delete-class-registry.repository.spec.ts
@@ -0,0 +1,86 @@
+import { BadRequestException } from "@/domain/@shared/exceptions";
+import Messages from "@/domain/@shared/util/messages";
+import { ClassRegistry } from "@/domain/class-registry/entity";
+import faker from "faker";
+import { Sequelize } from "sequelize-typescript";
+import { v4 as uuid } from "uuid";
+import { CourseModel, LessonModel } from "../../../course/model";
+import {
+  EnrollmentModel,
+  StudentClassModel,
+  StudentClassTeacherModel,
+} from "../../../student-class/model";
+import { StudentModel } from "../../../student/model";
+import { TeacherModel } from "../../../teacher/model";
+import {
+  ClassRegistryLessonModel,
+  ClassRegistryModel,
+  ClassRegistryStudentModel,
+} from "../../model";
+import { SequelizeCreateClassRegistryRepository } from "../create-class-registry.repostitory";
+
+import { SequelizeDeleteClassRegistryRepository } from "../delete-class-registry.repository";
+import { makeModels } from "./util";
+describe("Sequelize Delete Class Registry Repository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([
+      StudentClassModel,
+      CourseModel,
+      EnrollmentModel,
+      StudentClassTeacherModel,
+      TeacherModel,
+      LessonModel,
+      StudentModel,
+      ClassRegistryModel,
+      ClassRegistryStudentModel,
+      ClassRegistryLessonModel,
+    ]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("Fail trying delete a student class passing a wrong ID", async () => {
+    const repository = new SequelizeDeleteClassRegistryRepository();
+    const t = async () => {
+      await repository.delete(faker.random.word());
+    };
+    expect(t).rejects.toThrow(BadRequestException);
+    expect(t).rejects.toThrow(Messages.INVALID_ID);
+  });
+  it("Delete class registry", async () => {
+    const { course, student1, student2, teacher1, teacher2, studentClass } =
+      await makeModels();
+
+    const createRepository = new SequelizeCreateClassRegistryRepository();
+    const registry = new ClassRegistry(
+      uuid(),
+      studentClass.id,
+      new Date(),
+      teacher1.id,
+      [student1.id],
+      [course.lessons[0].id]
+    );
+
+    await createRepository.create(registry);
+
+    const repository = new SequelizeDeleteClassRegistryRepository();
+    await repository.delete(registry.id);
+
+    let exists = await ClassRegistryModel.findOne({
+      where: { id: registry.id },
+    });
+    expect(exists).toBeNull();
+  });
+});
