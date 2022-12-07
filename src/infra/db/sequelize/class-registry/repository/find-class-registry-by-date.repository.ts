249d5c397a24a5a055a3fe9cabeab9328ca76234
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-12-07 18:56:41 -0300

    feat: find class registry by date repository - validate id

--- src/infra/db/sequelize/class-registry/repository/find-class-registry-by-date.repository.ts
+++ src/infra/db/sequelize/class-registry/repository/find-class-registry-by-date.repository.ts
@@ -1,5 +1,8 @@
+import { BadRequestException } from "@/domain/@shared/exceptions";
+import Messages from "@/domain/@shared/util/messages";
 import { ClassRegistry } from "@/domain/class-registry/entity";
 import { FindClassRegitryByDateRepository } from "@/domain/class-registry/repository";
+import { validate } from "uuid";
 import { ClassRegistryModel } from "../model";
 
 export class SequelizeFindClassRegitryByDateRepository
@@ -9,6 +12,9 @@ export class SequelizeFindClassRegitryByDateRepository
     studentClassId: string,
     date: Date
   ): Promise<ClassRegistry | undefined> {
+    if (!validate(studentClassId)) {
+      throw new BadRequestException(Messages.INVALID_ID);
+    }
     const model = await ClassRegistryModel.findOne({
       where: {
         studentClassId: studentClassId,
