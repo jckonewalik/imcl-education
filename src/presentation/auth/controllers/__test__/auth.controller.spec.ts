80629c69f65b8d271560eabc43656888277fb353
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2022-12-26 16:32:17 -0300

    test: auth controller - signin

--- src/presentation/auth/controllers/__test__/auth.controller.spec.ts
+++ src/presentation/auth/controllers/__test__/auth.controller.spec.ts
@@ -1,15 +1,46 @@
 import Messages from "@/domain/@shared/util/messages";
+import { Role } from "@/domain/user/entity/role";
 import { UserModel, UserRoleModel } from "@/infra/db/sequelize/user/model";
 import { UserModule } from "@/modules/user.module";
 import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import faker from "faker";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { Sequelize } from "sequelize-typescript";
 import request from "supertest";
-
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { v4 as uuid } from "uuid";
+import { UserDTO } from "../../dto";
 
 jest.mock("firebase/auth");
+
+const makeUser = async ({
+  id = uuid(),
+  email = faker.internet.email(),
+  name = `${faker.name.firstName()} ${faker.name.lastName()}`,
+  active = true,
+}) => {
+  return await UserModel.create(
+    {
+      id,
+      email,
+      name,
+      active,
+      roles: [
+        {
+          userId: id,
+          role: Role.ROLE_USER,
+        },
+      ],
+    },
+    {
+      include: ["roles"],
+    }
+  );
+};
 describe("Auth Controller Tests", () => {
   let app: INestApplication;
 
@@ -37,6 +68,8 @@ describe("Auth Controller Tests", () => {
     }).compile();
     (getAuth as jest.Mock).mockResolvedValue({});
     (createUserWithEmailAndPassword as jest.Mock).mockResolvedValue({});
+    (signInWithEmailAndPassword as jest.Mock).mockResolvedValue({});
+
     app = moduleRef.createNestApplication();
     await app.init();
   });
@@ -76,5 +109,27 @@ describe("Auth Controller Tests", () => {
         expect(result.statusCode).toEqual(400);
         expect(result._body.message).toEqual(Messages.MISSING_PASSWORD);
       });
+  });
+
+  it(`/POST signin`, async () => {
+    const login = faker.internet.email();
+    const password = faker.internet.password();
+
+    const user = await makeUser({ email: login });
+
+    const result = await request(app.getHttpServer())
+      .post("/auth/signin")
+      .send({
+        login,
+        password,
+      });
+    expect(result.statusCode).toBe(200);
+    const body: UserDTO = result._body.body;
+    expect(body).toBeDefined();
+    expect(body).not.toBeNull();
+
+    expect(body.email).toBe(login);
+    expect(body.name).toBe(user.name);
+    expect(body.active).toBe(user.active);
   });
 });
